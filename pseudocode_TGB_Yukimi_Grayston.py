bac5c8ae3c206642a9c4dbae563e388639770862
yukimigrayston <107370518+yukimigrayston@users.noreply.github.com>
2022-11-09 08:49:45 -0600

    Add files via upload

---
+++ pseudocode_TGB_Yukimi_Grayston.py
@@ -0,0 +1,68 @@
+#Pseudocode TGB - Yukimi Grayston
+
+#Pseudocode for the textbased game 
+
+#when user opens program, program will ask the user to choose a class
+
+#if citizen is choosen, citizen ending will happen
+#elif goblin is choosen, goblin ending will happen
+#elif hero is choosen, player will get class options 
+#else print invalid input
+
+#if player has choosen hero
+#print spell caster, healer and fighter
+
+#if player chooses spell caster print a message telling the player their role
+#elif player chooses healer print a message telling the player their role
+#elif player chooses fighter print a message telling the player their role
+#else print invalid input
+
+#print room description
+
+#print options
+
+#if player leaves room print hallway description
+#elif players stay and search room they gain a key
+#add key to items list
+
+#if player is wizard and stays in room, player casts a light spell and finds key
+#add key to items list
+
+#when players leave room, print room description
+#print options left hallway, right hallway
+
+#if user goes to left hallway
+#user gains a code that can be used later
+#add code to items list
+#elif user goes to right hallway
+#goblin encounter plays
+#else print invalid response 
+
+#if user is spellcaster print spell options
+#elif user is healer print spell options
+#elif user is fighter print attack opetions
+#else invalid class
+
+#if user is spellscaster and chooses fireball
+#print fireball ending
+#else fight is won
+
+#if goblin encounter is won user goes to a room with three hallways
+#else print bad ending
+
+#if user chooses left hallway 2 they can use the code to gain treasure and leave
+#print ending 1
+#elif user chooses right hallway they fall into a trap
+#trap ending
+#elif user chooses middle hallway they find a trap door
+#else invalid response
+
+#if user goes down the trap door they find the final boss
+#elif user chooses not to go down they can choose the right or left hallway
+#else invalid response
+
+#if user fights boss
+#print off fighting actions for what class the player is
+
+#if user wins battle final treasure is added to items list
+#print true ending
